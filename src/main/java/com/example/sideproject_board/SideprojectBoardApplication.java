01414a1fe5ef8d5087b4e19b7f19bb641aa6a661
seongun Kim <seongun44@naver.com>
2024-07-26 15:54:15 +0900

    초기 설정 파일 업로드

---
+++ src/main/java/com/example/sideproject_board/SideprojectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.sideproject_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideprojectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideprojectBoardApplication.class, args);
+	}
+
+}
